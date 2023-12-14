53f52757ed8ab07411216a990fa7b7ea5f336a3f
unknown <korsh624@gmail.com>
2023-12-14 17:22:22 +0300

    add inicial

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
-sql_login='gb_statement'
-sql_name='gb_statement'
-sql_host='mysql96.1gb.ru'
-sql_pass='EPFcwj8D2-RW'
+sql_login='gb_ugmk'
+sql_name='gb_ugmk'
+sql_host='mysql94.1gb.ru'
+sql_pass='XyqbS49JX-pP'
 
 

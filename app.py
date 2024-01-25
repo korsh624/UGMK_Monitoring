f27c4d608b6bf1059f59595e0863392e3e7bfb91
arif228 <shukyrov.arif@gmail.com>
2024-01-25 18:58:42 +0300

    ggj

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from mysql.connector import connect
+import dbmodel
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,7 +9,22 @@ def home():
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html')
+    conn = connect(
+
+        user='gb_ugmk',
+
+        password='XyqbS49JX-pP',
+
+        host='mysql94.1gb.ru',
+
+        database='gb_ugmk',
+        ssl_disabled=True)
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM monitor')
+    bdinfo= cur.fetchall()
+    print(bdinfo)
+    conn.close()
+    return render_template('admin.html',bdinfo=bdinfo)
 
 @app.route('/testadmin')
 def testadmin():
